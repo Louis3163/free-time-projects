b3bfde77e061430a6bcf158baa35771424d618b7
Louis3163 <louis.scheffler06@gmail.com>
2026-05-14 15:24:09 +0200

    Verbesserungen

--- Spiele/BoxSimulator/src/App.java
+++ Spiele/BoxSimulator/src/App.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.lang.Thread;
 
 public class App {
     static Boolean Spielläuft = true;
@@ -22,6 +21,7 @@ public class App {
         int Ausdauer;
     }
     public static void main(String[] args) throws Exception {
+        Thread.sleep(1000);
         SpielStarten(scanner);
             for(int e = 0; e < 3; e++){
             KämpferArray[e] = new Kämpfer();
@@ -40,44 +40,48 @@ public class App {
     }
 
     public static void Stats(Scanner scanner){
+        clearTerminal();
         System.out.println("------Stats------");
         System.out.println("Name: " + Spieler.Name);
         System.out.println("HP: " + Spieler.HP);
         System.out.println("Ausdauer: " + Spieler.Ausdauer);
         System.out.println("Angriffsstärke: " + Spieler.Angriffsstärke);
         System.out.println("Schnelligkeit: " + Spieler.Schnelligkeit);
-        System.out.println("Um zurückzukehren gib 'zurueck' ein");
+        System.out.println("Um zurückzukehren drücke [Enter]");
         String Eingabe = scanner.nextLine();
         System.out.println(Eingabe);
-        if(Eingabe.equals("zurueck")){
+        if(Eingabe.equals("")){
             Menü(scanner);
         }
 
     }
 
     public static void Menü(Scanner scanner){
-        System.out.println("Mit folgenden Befehlen kannst du mit der Spielwelt interagieren:");
-        System.out.println("Stats - damit kannst du dir die Stats deines Charakters ansehen");
-        System.out.println("Arena - damit kannst du in die Arene gehen und an einem Kampf teilnehmen");
+        clearTerminal();
+        System.out.println("Was willst du tun?");
+        System.out.println("[1]Stats - damit kannst du dir die Stats deines Charakters ansehen");
+        System.out.println("[2]Arena - damit kannst du in die Arene gehen und an einem Kampf teilnehmen");
         String Eingabe = scanner.nextLine();
         switch(Eingabe){
-            case "Stats": Stats(scanner);
+            case "1": Stats(scanner);
                 break;
-            case "Arena": Arena(scanner);
+            case "2": Arena(scanner);
+                break;
+            default: System.out.println("Ungültige Eingabe, bitte versuche es erneut.");
+                Menü(scanner);
                 break;
         }
     }
     public static void Arena(Scanner scanner){
+        clearTerminal();
         System.out.println("Das sind deine Gegner um einen auszuwählen gib eine Zahl von 1-3 ein:");
         for(int k = 0; k < 3; k++){
             System.out.println("-------------------------------------");
-            System.out.println(KämpferArray[1].Name);
             System.out.println("Name: " + KämpferArray[k].Name);
             System.out.println("HP: " + KämpferArray[k].HP);
             System.out.println("Ausdauer: " + KämpferArray[k].Ausdauer);
             System.out.println("Angriffsstärke: " + KämpferArray[k].Angriffsstärke);
             System.out.println("Schnelligkeit: " + KämpferArray[k].Schnelligkeit);
-            System.out.println("Ausdauer: " + KämpferArray[k].Name);
         }
         String Eingabe = scanner.nextLine();
             switch(Eingabe){
@@ -108,28 +112,32 @@ public class App {
         return Name;
     }
     public static void Kampf(int Kämpfernummer){
+        clearTerminal();
         while(Spielläuft){
             System.out.println("Was willst du tun?");
-            System.out.println("treten");
-            System.out.println("schlagen");
+            System.out.println("[1]treten");
+            System.out.println("[2]schlagen");
             String Eingabe = scanner.nextLine();
             int Glück = random.nextInt(2) + 1;
             switch(Eingabe){
-                case "treten":  if(Glück == 1){
+                case "1":  if(Glück == 1){
                                 System.out.println("Du hast " + KämpferArray[Kämpfernummer].Name + "getreten!");
                                 KämpferArray[Kämpfernummer].HP =+ Spieler.Angriffsstärke;}
                                 else{
                                 System.out.println("Du hast versucht " + KämpferArray[Kämpfernummer].Name + "zu treten, aber er konnte ausweichen!");
                                 }
                             break;
-                case "schlagen": if(Glück == 1){
+                case "2": if(Glück == 1){
                                 System.out.println("Du hast " + KämpferArray[Kämpfernummer].Name + "geschlagen!");
                                 KämpferArray[Kämpfernummer].HP =+ Spieler.Angriffsstärke;}
                                 else{
                                     System.out.println("Du hast versucht " + KämpferArray[Kämpfernummer].Name + "zu schlagen, aber er konnte ausweichen!");
                                 }
                             break;
+                default: System.out.println("Du machst nichts...");
+                    break;
             }
+            pausieren(1000);
             int Reaktion = random.nextInt(2) + 1;
             switch (Reaktion) {
                 case 1: System.out.println(KämpferArray[Kämpfernummer].Name + ": Autsch!");
@@ -139,6 +147,7 @@ public class App {
                 default: System.out.println(KämpferArray[Kämpfernummer].Name + " beleidigt dich");
                     break;
             }
+            pausieren(1000);
             int GegnerAngriff = random.nextInt(2);
             Glück = random.nextInt(2) +1;
             switch(GegnerAngriff){
@@ -164,5 +173,16 @@ public class App {
         else{
             System.out.println("Du hast gewonnen!");
         }
+    }
+    public static void pausieren(int Zeit){
+        try {
+            Thread.sleep(Zeit);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void clearTerminal(){
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
     }
 }
